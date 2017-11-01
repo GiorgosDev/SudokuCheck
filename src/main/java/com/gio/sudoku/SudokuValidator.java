ca9a8e0caca36aa1675539b4d4ee2dad1f5aa0cb
giorgos <mayshmaz@gmail.com>
2017-11-01 21:14:25 +0200

    column sum check added

--- src/main/java/com/gio/sudoku/SudokuValidator.java
+++ src/main/java/com/gio/sudoku/SudokuValidator.java
@@ -1,9 +1,14 @@
 package com.gio.sudoku;
 
+import java.util.stream.IntStream;
+
 public class SudokuValidator {
 
     public boolean validate (int n){
         return n>0 & n<10;
     }
 
+    public boolean checkColumn(int [] column) {
+        return IntStream.of(column).sum() == 45;
+    }
 }
--- src/test/java/com/gio/sudoku/SudokuValidatorTest.java
+++ src/test/java/com/gio/sudoku/SudokuValidatorTest.java
@@ -8,7 +8,7 @@ package com.gio.sudoku;
 //todo  # - validate column duplicates
 //todo  # - validate duplicates parallel implementation
 //todo  # - validate while reading ?
-
+//todo  # - small type
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,6 +22,17 @@ public class SudokuValidatorTest {
         Assert.assertEquals(false, validator.validate(10));
         Assert.assertEquals(false, validator.validate(0));
         Assert.assertEquals(false, validator.validate(-1));
+    }
 
+    @Test
+    public void testColumnSum(){
+        int [] columnCorrect = new int [] {1, 2 ,3, 4, 5, 6, 7, 8, 9};
+        int [] columnMore = new int [] {1, 2 ,3, 4, 5, 6, 7, 9, 9};
+        int [] columnLess = new int [] {1, 2 ,3, 4, 5, 6, 7, 1, 1};
+
+        SudokuValidator validator = new SudokuValidator();
+        Assert.assertEquals( true, validator.checkColumn(columnCorrect));
+        Assert.assertEquals( false, validator.checkColumn(columnMore));
+        Assert.assertEquals( false, validator.checkColumn(columnLess));
     }
 }
