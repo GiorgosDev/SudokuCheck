47d565102083fab9ddc925dd85900f949c9286f4
giorgos <mayshmaz@gmail.com>
2017-11-04 19:38:50 +0200

    error codes added

--- src/main/java/com/gio/sudoku/SudokuConstants.java
+++ src/main/java/com/gio/sudoku/SudokuConstants.java
@@ -9,4 +9,21 @@ public class SudokuConstants {
     public static final String SUDOKU_CSV_SEPARATOR = ",";
     public static final String VALID_RESPONSE = "VALID";
     public static final String INVALID_RESPONSE = "INVALID";
+    public static final String PROCESSED = "PROCESSED";
+    public static final String ERROR = "ERROR";
+
+    public static final String ERR_FIELD_SIZE = "Field size should be 9x9";
+    public static final String ERR_NO_ARG = "Please provide input file name as argument";
+    public static final String ERR_FILE_NOT_FOUND = "File %s was not found";
+    public static final String ERR_INCORRECT_ROW_LENGTH = "Row %d has incorrect length";
+    public static final String ERR_INCORRECT_CHAR = "Incorrect character in row %d";
+    public static final String ERR_ROW_ELEMENT_OUT_OF_RANGE = "Element out of range in row %d";
+    public static final String ERR_COL_ELEMENT_OUT_OF_RANGE = "Element out of range in column %d";
+    public static final String ERR_BOX_ELEMENT_OUT_OF_RANGE = "Element out of range in box %d";
+    public static final String ERR_INCORRECT_ROW_SUM = "Incorrect sum in row %d";
+    public static final String ERR_INCORRECT_COL_SUM = "Incorrect sum in column %d";
+    public static final String ERR_INCORRECT_BOX_SUM = "Incorrect sum in box %d";
+    public static final String ERR_DUPLICATE_IN_ROW = "Duplicate in row %d";
+    public static final String ERR_DUPLICATE_IN_COL = "Duplicate in column %d";
+    public static final String ERR_DUPLICATE_IN_BOX = "Duplicate in box %d";
 }
